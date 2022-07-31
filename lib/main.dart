aebf0f30b85ef76c7e3b50abdb51c5aa3a3e0d89
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-07-31 11:26:09 +0530

    Theme Extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:first_app/utils/routes.dart';
+import 'package:first_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -20,17 +21,13 @@ class MyApp extends StatelessWidget {
 
     bringingVegetables(thaila: true);
 
+
     return MaterialApp(
       // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily),
-      darkTheme: ThemeData(
-        brightness: Brightness.light,
-        // primarySwatch: Colors.red
-      ),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => const LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+       title: const Text("Catalog App"),
       ),
       body: Center(
         child: Center(
